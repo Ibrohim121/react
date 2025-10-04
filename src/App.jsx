800a4c894816f83955727188fc159cc8532481c3
Ibrohim121 <igofurjonov229@gmail.com>
2025-10-04 15:25:53 +0500

    react4

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,22 @@
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import SlugPage from "./assets/slug";
-
-
-const mockData = [
-  {
-    id: 1,
-    title: "salom nmadr",
-    slug: "salom nmadr",
-    image: "/src/assets/R.jpg"
-  },
-  {
-    id: 2,
-    title: "salom nmadr",
-    slug: "salom nmadr",
-    image: "/src/assets/W.jpg"
-  },
-  {
-    id: 3,
-    title: "salom nmadr",
-    slug: "salom nmadr",
-    image: "/src/assets/R.jpg"
-  },
-  {
-    id: 4,
-    title: "salom nmadr",
-    slug: "salom nmadr",
-    image: "/src/assets/W.jpg"
-  },
-];
+import Button from "./assets/button"; 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>Kurslar Royxati</h1>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          {mockData.map((item) => (
-            <li key={item.id} style={{ marginBottom: "20px" }}>
-              <Link to={`/courses/${item.slug}`}>
-                <img src={item.image} alt={item.title} width="150" />
-                <h3>{item.title}</h3>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <Routes>
-          <Route path="/courses/:slug" element={<SlugPage data={mockData} />} />
-        
-          
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="p-6 space-x-4">
+      <Button>Default</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+      <Button size="sm">Small</Button>
+      <Button size="lg">Large</Button>
+    </div>
   );
 }
 
 export default App;
+
+
 
