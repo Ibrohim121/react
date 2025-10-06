1ad3fc76f6529b912b0fc738a5b94653a5610b3e
Ibrohim121 <igofurjonov229@gmail.com>
2025-10-06 22:57:17 +0500

    react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 
-import React from "react";
-import Button from "./assets/button"; 
+import React from 'react';
+import ShoppingCart from './ShoppingCart';
+import ImageSlider from './ImageSlider';
 
 function App() {
   return (
-    <div className="p-6 space-x-4">
-      <Button>Default</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="link">Link</Button>
-      <Button size="sm">Small</Button>
-      <Button size="lg">Large</Button>
+    <div style={{ padding: '20px' }}>
+      <ShoppingCart />
+      <ImageSlider />
     </div>
   );
 }
 
 export default App;
+
 
 
 
